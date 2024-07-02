d78d6c3aa2a7c491cde4bafc579b533ee741feb1
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-07-02 12:59:38 +0100

    initial commit

---
+++ activities.js
@@ -0,0 +1,18 @@
+// ACTIVITY 01
+
+// let age = 19;
+
+// if (age <= 18) { 
+//     console.log('Can not drink')
+// }
+//     else { console.log('Can drink')
+// }
+
+// ACTIVITY 02
+
+let Topping = "Chicken" ;
+
+switch (Topping) {
+    case Topping == "Chicken":
+    console.log("This should be on the pizza.")
+}
---
+++ ifelse.js
@@ -0,0 +1,41 @@
+// let weather= "Sunny" ;
+
+// if (weather == "Sunny") {
+//     console.log("I'd better put on suncream")
+// }
+
+// else if (weather == "Rainy") {
+//     connsole.log("I'd better take an umbrella")
+// }
+
+// else if (weather == "Dull") {
+//     console.log("I'd better wear a jumper")
+// }
+
+// else {console.log ("It could go either way")
+// }
+
+// let place = "Manchester" ;
+// let weather = "Dull" ;
+
+// if (place == "Manchester" && weather == "Sunny") {
+//     console.log ("Check again...")
+// }
+
+// else if (place == "Manchester" && weather == "Rainy") {
+//     console.log("Obviously...")
+// }
+
+// else if (place == "Manchester" && weather == "Dull") {
+//     console.log ("Oh great, it's dull.")
+// }
+
+// else {console.log("What it isn't raining?")}
+
+// let day = "Sunday" ;
+
+// if (day == "Saturday" || day == "Sunday"){
+//     console.log ("It's the weekend!")
+// }
+
+// else {console.log ("It's NOT the weekend!")}
