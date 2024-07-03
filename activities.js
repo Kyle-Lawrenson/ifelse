e1d8218981702b26cd95f5ea4acddc9f7f9a3e3b
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-07-03 14:58:32 +0100

    add activity

--- activities.js
+++ activities.js
@@ -28,11 +28,100 @@
 
 // ACTIVITY 03
 
-// let password = "pizzatime"
+// let password = "pass"
 
-// if (password >= 8) {
+// if (password.length < 8) {
 //     console.log ("Too short.")
 // }
+
+// else {console.log ("Confirm password")}
+
+// ACTIVITY 04
+
+// if (num % 3 === 0 && num % 5 === 0) {
+//     console.log("Fizz Buzz")
+// }
+
+// else if (num % 3 === 0) {
+//     console.log("Fizz")
+// }
+
+// else if (num % 5 === 0) {
+//     console.log("Buzz")
+// }
+
+// else {console.log(num)}
+
+// ACTIVITY 05
+
+// let num = 8008;
+// let numStr = num.toString();
+// let revStr = numStr.split(' ').reverse().join ('');
+// if (numStr === revStr) {
+//     console.log(`${num} It is a palindrome. `);
+// }
+
+// else { console.log(`${num} It is not a palindrome. `)
+// }
+
+// ACTIVITY 06
+
+    // let time = 3;
+    // let placeOfWork = "the office";
+    // let townOfHome = "home";
+     
+    // if (time >= 0 && time < 6) {
+    //     console.log(`At ${time}:00, I'm trying to sleep.`);
+    // } else if (time >= 6 && time < 8) {
+    //     console.log(`At ${time}:00, I'm at ${townOfHome}.`);
+    // } else if (time >= 8 && time < 9) {
+    //     console.log(`At ${time}:00, I'm commuting.`);
+    // } else if (time >= 9 && time < 17) {
+    //     console.log(`At ${time}:00, I'm at ${placeOfWork}.`);
+    // } else if (time >= 17 && time < 18) {
+    //     console.log(`At ${time}:00, I'm on my way home.`);
+    // } else if (time >= 18 && time < 24) {
+    //     console.log(`At ${time}:00, I'm at ${townOfHome}.`);
+    // } else {
+    //     console.log(`That's an invalid time.`);
+    // }
+
+// ACTIVITY 07
+
+//     let str = "jrfndklhgfndjkjlkgperfijfhdknsadcvjhiiohjfkledsopiuhgtyujwsdxcvhgfdjhiopiwquhejkdsoiufghedjwsh";
+// const vowels = "aeiouAEIOU";
+// let lastVowelIndex = -1;
+ 
+// for (let i = str.length - 1; i >= 0; i--) {
+//     if (vowels.includes(str[i])) {
+//         lastVowelIndex = i;
+//         break;
+//     }
+// }
+ 
+// console.log(`The index of the last vowel is: ${lastVowelIndex}`);
+
+// ACTIVITY 08
+
+// let word = "bob";
+// let isSame = word.length > 0 && word[0] === word[word.length - 1];
+// console.log(isSame);
+
+// ACTIVITY 09
+
+// let num1 = 4;
+// let num2 = 5;
+ 
+// let sum = num1 + num2;
+ 
+// let result;
+// if (sum % 2 === 0) {
+//     result = sum;
+// } else {
+//     result = num1 * num2;
+// }
+ 
+// console.log(result);
 
 // ARRAY ACTIVITY 01
 
